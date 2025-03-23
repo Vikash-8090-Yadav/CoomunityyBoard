58d434ac29759eccddcef227a44c61b9a99e526b
Vikash Yadav <yadav621gzp@gmail.com>
2025-03-24 01:05:16 +0530

    update: Next.js configuration with new build settings

--- Frontend/next.config.ts
+++ Frontend/next.config.ts
@@ -2,6 +2,9 @@ import  { NextConfig } from "next";
 
 const nextConfig = {
   /* config options here */
+  images :{
+    domains :["ipfs.infura.io","sal-dapp.infura-ipfs.io"]
+  }
 };
 
 export default nextConfig;
