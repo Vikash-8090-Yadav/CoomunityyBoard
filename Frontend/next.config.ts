17948b1cf187eadf613e82356c2174c7b3afe995
Vikash Yadav <yadav621gzp@gmail.com>
2025-04-07 18:42:25 +0530

    chore: remove deprecated files after Next.js 15 migration

--- Frontend/next.config.ts
+++
@@ -1,9 +0,0 @@
-import { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  images: {
-    domains: ["ipfs.infura.io", "sal-dapp.infura-ipfs.io"]
-  }
-};
-
-export default nextConfig;
